f353e70597c08c2459b44fa4ba77b2183b61eef1
KyleM73 <kyle.morgenstein@gmail.com>
2023-10-04 15:49:41 -0400

    better plotting

--- generative_minimal/train.py
+++ generative_minimal/train.py
@@ -11,6 +11,8 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu" # mps is almost always s
 def imshow(img):
     img = img / 2 + 0.5 # unnormalize
     plt.imshow(np.transpose(img.numpy(), (1,2,0)))
+    plt.axis("off")
+    plt.tight_layout()
     plt.show() 
 
 if __name__ == "__main__":
