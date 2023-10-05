c2365a4580d8aa8739738b02af828aa9ce9c3295
KyleM73 <kyle.morgenstein@gmail.com>
2023-10-05 08:16:48 -0500

    added comments + better printing

--- generative_minimal/train.py
+++ generative_minimal/train.py
@@ -2,7 +2,7 @@ import torch
 import torchvision
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu" # mps is almost always slower
-if DEVICE == "cuda": torch.backends.cudnn.benchmark = True
+if DEVICE == "cuda": torch.backends.cudnn.benchmark = True # enables cuDNN auto-tuner
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -17,9 +17,6 @@ def imshow(img):
     plt.show() 
 
 if __name__ == "__main__":
-    # params
-    batch_size = 1000
-
     # CIFAR10
     """
     transform = torchvision.transforms.Compose([
@@ -29,10 +26,12 @@ if __name__ == "__main__":
     trainset = torchvision.datasets.CIFAR10(root="./data", train=True, download=False, transform=transform) # 50k
     testset = torchvision.datasets.CIFAR10(root="./data", train=False, download=False, transform=transform) # 10k
     classes = ("plane", "car", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck")
+    n_classes = len(classes)
     in_channels = 3
     in_size = 32
-    latent_dim = 8
+    latent_dim = 128
     epochs = 10
+    batch_size = 200
     """
 
     # MNIST
@@ -43,10 +42,12 @@ if __name__ == "__main__":
     trainset = torchvision.datasets.MNIST(root="./data", train=True, download=False, transform=transform) # 60k
     testset = torchvision.datasets.MNIST(root="./data", train=False, download=False, transform=transform) # 10k
     classes = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+    n_classes = len(classes)
     in_channels = 1
     in_size = 28
-    latent_dim = 16
+    latent_dim = 32
     epochs = 10
+    batch_size = 200
 
     # make dataset
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
@@ -58,7 +59,7 @@ if __name__ == "__main__":
     kld_weight_test = 1/len(testloader)
     
     # define network
-    net = CVAE(in_size=in_size, in_channels=in_channels, latent_dim=latent_dim, context_dim=len(classes)).to(DEVICE)
+    net = CVAE(in_size=in_size, in_channels=in_channels, latent_dim=latent_dim, context_dim=n_classes).to(DEVICE)
     optimizer = torch.optim.Adam(net.parameters(), lr=3e-4)
 
     print(net)
@@ -82,13 +83,18 @@ if __name__ == "__main__":
             running_recons += loss_dict["reconstruction"]
             running_kld += loss_dict["kld"]
 
-            print(
-                "[{epoch}, {batch}] loss: {loss} reconstruction loss: {recons} kld loss: {kld}"
-                .format(epoch=epoch,batch=i,loss=loss_dict["loss"].detach(),recons=loss_dict["reconstruction"],kld=loss_dict["kld"])
+            if (i/len(trainloader)*100 % 10) < 0.1:
+                print(
+                    "[{epoch}, {batch}%] loss: {loss} reconstruction loss: {recons} kld loss: {kld}"
+                    .format(epoch=epoch+1,batch=int(i/len(trainloader)*100),loss=loss_dict["loss"].detach(),recons=loss_dict["reconstruction"],kld=loss_dict["kld"])
                 )
         print(
+            "[{epoch}, {batch}%] loss: {loss} reconstruction loss: {recons} kld loss: {kld}"
+            .format(epoch=epoch+1,batch=100,loss=loss_dict["loss"].detach(),recons=loss_dict["reconstruction"],kld=loss_dict["kld"])
+        )
+        print(
             "[{epoch}, train] loss: {loss} reconstruction loss: {recons} kld loss: {kld}"
-            .format(epoch=epoch,loss=running_loss/(i+1),recons=running_recons/(i+1),kld=running_kld/(i+1))
+            .format(epoch=epoch+1,loss=running_loss/(i+1),recons=running_recons/(i+1),kld=running_kld/(i+1))
         )
         net.eval()
         running_loss, running_recons, running_kld = 0, 0, 0
@@ -103,10 +109,10 @@ if __name__ == "__main__":
             running_kld += loss_dict["kld"]
         print(
             "[{epoch}, test] loss: {loss} reconstruction loss: {recons} kld loss: {kld}"
-            .format(epoch=epoch,loss=running_loss/(i+1),recons=running_recons/(i+1),kld=running_kld/(i+1))
+            .format(epoch=epoch+1,loss=running_loss/(i+1),recons=running_recons/(i+1),kld=running_kld/(i+1))
         )
         print()
 
-    labels = torch.tensor([[i for _ in range(len(classes))] for i in range(len(classes))]).view(-1)
-    imgs = net.cpu().sample(batch_size=100, context=torch.nn.functional.one_hot(labels))
-    imshow(torchvision.utils.make_grid(imgs, nrow=10))
+    labels = torch.tensor([[i for _ in range(n_classes)] for i in range(n_classes)]).view(-1)
+    imgs = net.cpu().sample(batch_size=n_classes**2, context=torch.nn.functional.one_hot(labels))
+    imshow(torchvision.utils.make_grid(imgs, nrow=n_classes))
